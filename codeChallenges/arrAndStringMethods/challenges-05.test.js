16f1e51ca8f6711ac09e2ac580fabf49ea9f5ffe
Chase Tucker <tuck8106@gmail.com>
2019-11-03 21:55:46 -0800

    7 complete

--- codeChallenges/arrAndStringMethods/challenges-05.test.js
+++ codeChallenges/arrAndStringMethods/challenges-05.test.js
@@ -163,7 +163,13 @@ removeLastCharacters('Gregor', 9) returns ''
 ------------------------------------------------------------------------------------------------ */
 
 const removeLastCharacters = (str, numberOfCharacters) => {
-  // Solution code here...
+  if(numberOfCharacters > str.length) return '';
+  else if(numberOfCharacters < 0) return str;
+  else {
+    let tempArr = str.split('');
+    tempArr.splice((-numberOfCharacters));
+    return tempArr.join('');
+  }
 };
 
 
