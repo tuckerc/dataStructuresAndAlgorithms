a5a207fb03098824734d07d0137888a66171b060
Chase Tucker <tuck8106@gmail.com>
2020-01-06 22:18:57 -0800

    added comments

--- code401Challenges/ArrayReverse.java
+++ code401Challenges/ArrayReverse.java
@@ -1,20 +1,36 @@
 public class ArrayReverse {
   public static void main(String[] args) {
+    // array to be used for testing
     int[] testArr = {1, 2, 3, 4, 5};
+
+    /* run arrayReverse with testArr as argument
+    ** and assign the value to testArr */
     testArr = arrayReverse(testArr);
+
+    // print reversed array
     for(int el : testArr) {
       System.out.print(el + " ");
     }
     System.out.println();
   }
 
+  /***********************************************
+  ** Method used to reverse the values of an array
+  ** of integers and return the new array
+  ***********************************************/
   public static int[] arrayReverse(int[] arr) {
-    int[] returnArr = new int[arr.length];
+    // create new array for storing reversed array
+    int[] reversedArr = new int[arr.length];
+    /* integer to be used to keep track of 
+    ** position in reversedArr */
     int idx = 0;
+
+    /* loop backward through input array and assign
+    ** each value to reversedArr at idx */
     for(int i = arr.length - 1; i >= 0; i--) {
-      returnArr[idx] = arr[i];
+      reversedArr[idx] = arr[i];
       idx++;
     }
-    return returnArr;
+    return reversedArr;
   }
 }
