b0c6c61248fef7aca9750e00a1d91f7811fa8f67
Chase Tucker <tuck8106@gmail.com>
2020-01-09 10:27:22 -0800

    binarySearch complete

---
+++ code401Challenges/src/test/java/code401Challenges/BinarySearchTest.java
@@ -0,0 +1,52 @@
+package code401Challenges;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class BinarySearchTest {
+
+  @Test
+  public void testBinarySearchHappy() {
+    int key = 5;
+    int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int expected = 4;
+    int actual = BinarySearch.binarySearch(testArray, key);
+    assertEquals(expected, actual);
+
+    key = 400;
+    expected = -1;
+    actual = BinarySearch.binarySearch(testArray, key);
+    assertEquals(expected, actual);
+
+  }
+
+  @Test
+  public void testBinarySearchFail() {
+    int key = 5;
+    int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int expected = 99;
+    int actual = BinarySearch.binarySearch(testArray, key);
+    assertNotEquals("Should not return " + expected, actual);
+  }
+
+  @Test
+  public void testBinarySearchEdge1() {
+    int key = 5;
+    int[] testArray = {};
+    int expected = -1;
+    int actual = BinarySearch.binarySearch(testArray, key);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testBinarySearchEdge2() {
+    int key = 5;
+    int[] testArray = {5};
+    int expected = 0;
+    int actual = BinarySearch.binarySearch(testArray, key);
+    assertEquals(expected, actual);
+  }
+}
