c748575d38a308fb173a15772559b0db22bd14d6
Chase Tucker <tuck8106@gmail.com>
2020-01-15 10:30:13 -0800

    committing before deleting ll-insertion-kth-from-end branch

--- code401Challenges/src/test/java/code401Challenges/LinkedListTest/LinkedListTest.java
+++ code401Challenges/src/test/java/code401Challenges/LinkedListTest/LinkedListTest.java
@@ -8,84 +8,95 @@ import static org.junit.Assert.*;
 
 public class LinkedListTest {
 
-  LinkedList<Integer> testLinkedList;
+  LinkedList<Integer> singleValueLinkedList;
   Integer newValue;
+
+  LinkedList<Integer> emptyList;
+  
+  LinkedList<Integer> multipleValueList;
 
   @Before
   public void setup() {
-    testLinkedList = new LinkedList<>();
+    singleValueLinkedList = new LinkedList<>();
     newValue = 5;
-    testLinkedList.insert(newValue);
-  }
-
-  @Test
-  public void testLinkedListInstantiation() {
-    assertNull(testLinkedList.getHead().getNext());
-  }
-
-  @Test
-  public void testLinkedListInsert() {
-    assertEquals(newValue, testLinkedList.getHead().getValue());
-  }
-
-  @Test
-  public void testLinkedListInsertMultiple() {
+    singleValueLinkedList.insert(newValue);
+    
+    emptyList = new LinkedList<>();
+    
+    multipleValueList = new LinkedList<>();
     newValue = 6;
-    testLinkedList.insert(newValue);
-    assertEquals(newValue, testLinkedList.getHead().getValue());
-  }
-
-  @Test
-  public void testLinkedListIncludesTrue() {
-    assertTrue(testLinkedList.includes(newValue));
-  }
-
-  @Test
-  public void testLinkedListIncludesFalse() {
-    assertFalse(testLinkedList.includes(12));
-  }
-
-  @Test
-  public void testLinkedListToString() {
-    newValue = 6;
-    testLinkedList.insert(newValue);
-    String expected = "( 6 ) -> ( 5 ) -> NULL";
-    assertEquals(expected, testLinkedList.toString());
-  }
-
-  @Test
-  public void testLinkedListAppend() {
+    multipleValueList.insert(newValue);
     newValue = 8;
-    testLinkedList.append(newValue);
-    Node<Integer> currentNode = testLinkedList.getHead();
-    while ( currentNode.getNext() != null) {
-      currentNode = currentNode.getNext();
-    }
-    assertEquals(newValue, currentNode.getValue());
-  }
-
-  @Test
-  public void testLinkedListInsertBefore() {
-    newValue = 6;
-    testLinkedList.insert(newValue);
-    newValue = 8;
-    testLinkedList.append(newValue);
+    multipleValueList.append(newValue);
     newValue = 12;
-    testLinkedList.insertBefore(newValue, 5);
-    String expected = "( 6 ) -> ( 12 ) -> ( 5 ) -> ( 8 ) -> NULL";
-    assertEquals(expected, testLinkedList.toString());
+    multipleValueList.insertBefore(newValue, (Integer) 8);
+    newValue = 5;
+    multipleValueList.insertAfter(newValue, (Integer) 6);
   }
 
   @Test
-  public void testLinkedListInsertAfter() {
-    newValue = 6;
-    testLinkedList.insert(newValue);
-    newValue = 8;
-    testLinkedList.append(newValue);
-    newValue = 12;
-    testLinkedList.insertAfter(newValue, 5);
+  public void emptyLinkedListInstantiation() {
+    assertNull(emptyList.getHead());
+  }
+
+  @Test
+  public void singleValueLinkedListInsert() {
+    assertEquals((Integer) 5, singleValueLinkedList.getHead().getValue());
+  }
+
+  @Test
+  public void multipleValueLinkedListInsertMultiple() {
+    newValue = 37;
+    multipleValueList.insert(newValue);
+    assertEquals(newValue, multipleValueList.getHead().getValue());
+  }
+
+  @Test
+  public void multipleValueLinkedListIncludesTrue() {
+    assertTrue(multipleValueList.includes(8));
+  }
+
+  @Test
+  public void multipleValueLinkedListIncludesFalse() {
+    assertFalse(multipleValueList.includes(120));
+  }
+
+  @Test
+  public void multipleValueLinkedListToString() {
     String expected = "( 6 ) -> ( 5 ) -> ( 12 ) -> ( 8 ) -> NULL";
-    assertEquals(expected, testLinkedList.toString());
+    assertEquals(expected, multipleValueList.toString());
+  }
+
+  @Test
+  public void multipleValueLinkedListAppend() {
+    assertEquals((Integer) 8, multipleValueList.kthFromEnd(0));
+  }
+
+  @Test
+  public void multipleValueLinkedListInsertBefore() {
+    assertEquals((Integer) 12, multipleValueList.kthFromEnd(1));
+  }
+
+  @Test
+  public void multipleValueLinkedListInsertAfter() {
+    assertEquals((Integer) 5, multipleValueList.kthFromEnd(2));
+  }
+
+  @Test
+  public void multipleValueLinkedListKthFromEnd() {
+    assertEquals((Integer) 8, multipleValueList.kthFromEnd(0));
+    assertEquals((Integer) 12, multipleValueList.kthFromEnd(1));
+    assertEquals((Integer) 6, multipleValueList.kthFromEnd(3));
+  }
+
+  @Test
+  public void setSingleValueLinkedListKthFromEnd() {
+    assertEquals((Integer) 5, singleValueLinkedList.kthFromEnd(0));
+  }
+
+  @Test (expected = Exception.class)
+  public void singleValueLinkedListKthFromEndException() {
+    singleValueLinkedList.kthFromEnd(99);
   }
 
 }
